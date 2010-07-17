80e1f6ba91f53ea39724478a168ea118a2e7823a
pemryan@gmail.com <pemryan@gmail.com@7bf8055b-b726-ec87-711b-5acdc8c35925>
2010-07-17 13:28:23 +0000

    AddFile: AI3_4记忆的细胞和分子机制.mm (early version from LQQ)

---
+++ trunk/discussions/AI3_4记忆的细胞和分子机制.mm
@@ -0,0 +1,35 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1277881596201" ID="ID_1303639017" MODIFIED="1277882196170" TEXT="&#x8bb0;&#x5fc6;&#x7684;&#x7ec6;&#x80de;&#x548c;&#x5206;&#x5b50;&#x673a;&#x5236;">
+<node CREATED="1277881741545" ID="ID_1730626095" MODIFIED="1277882491998" POSITION="right" TEXT="&#x751f;&#x7269;&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x7684;&#x7ec4;&#x6210;&#xff1a;&#x4e00;&#x4e9b;&#x57fa;&#x672c;&#x6982;&#x5ff5;"/>
+<node CREATED="1277882504342" ID="ID_352565918" MODIFIED="1277882528998" POSITION="right" TEXT="&#x795e;&#x7ecf;&#x7f51;&#x7edc;&#x4e2d;&#x4fe1;&#x606f;&#x7684;&#x8868;&#x5f81;">
+<node CREATED="1277882533232" ID="ID_208691093" MODIFIED="1277882614513" TEXT="&#x751f;&#x547d;&#x7684;&#x7ecf;&#x6d4e;&#x6027;&#x539f;&#x5219;"/>
+<node CREATED="1277882628795" ID="ID_621552868" MODIFIED="1277882694045" TEXT="&#x52a8;&#x4f5c;&#x7535;&#x4f4d;&#x7684;&#x4ea7;&#x751f;"/>
+<node CREATED="1277882699435" ID="ID_1063579788" MODIFIED="1277882710420" TEXT="&#x52a8;&#x4f5c;&#x7535;&#x4f4d;&#x7684;&#x4f20;&#x9012;"/>
+<node CREATED="1277883686232" ID="ID_156722805" MODIFIED="1277883747529" TEXT="&#x7a81;&#x89e6;&#x4e2d;&#x7684;&#x4fe1;&#x606f;&#x4f20;&#x9012;">
+<node CREATED="1277883750701" ID="ID_1635022358" MODIFIED="1277883817873" TEXT="&#x795e;&#x7ecf;&#x9012;&#x8d28;&#x53ca;&#x53d7;&#x4f53;"/>
+<node CREATED="1277883821295" ID="ID_102512695" MODIFIED="1277898944935" TEXT="&#x6811;&#x7a81;&#x8ba1;&#x7b97;">
+<node COLOR="#ff3399" CREATED="1277898994982" ID="ID_15643559" LINK="&#x795e;&#x7ecf;&#x5143;&#x4e0e;&#x7f8e;&#x56fd;&#x9009;&#x4e3e;&#x56e2;.pdf" MODIFIED="1277899087701" STYLE="bubble" TEXT="&#xff08;&#x8fd9;&#x4e00;&#x5757;&#x633a;&#x6709;&#x610f;&#x601d;&#xff0c;&#x4f46;&#x6211;&#x672c;&#x4eba;&#x4e86;&#x89e3;&#x4e0d;&#x591a;&#x3002;&#x6709;&#x5174;&#x8da3;&#x7684;&#x53ef;&#x4ee5;&#x770b;&#x8fd9;&#x91cc;&#xff09;"/>
+</node>
+<node CREATED="1277883855326" ID="ID_851029825" MODIFIED="1277883972170" TEXT="&#x795e;&#x7ecf;&#x75c5;&#x53ca;&#x6bd2;&#x54c1;"/>
+</node>
+</node>
+<node CREATED="1277883998138" ID="ID_1043954098" MODIFIED="1277899172795" POSITION="right" TEXT="&#x8bb0;&#x5fc6;">
+<node CREATED="1277884210810" ID="ID_401276198" MODIFIED="1277884333013" TEXT="&#x4ece;&#x751f;&#x7269;&#x7535;&#x6765;&#x770b;">
+<node CREATED="1277884359342" ID="ID_1761618496" MODIFIED="1277884376498" TEXT="LTP&#x3001;LTD"/>
+<node CREATED="1277884381810" ID="ID_583785366" MODIFIED="1277884387357" TEXT="&#x4e3e;&#x4f8b;"/>
+<node COLOR="#6633ff" CREATED="1277884394951" ID="ID_42380967" MODIFIED="1277899550560" STYLE="bubble" TEXT="Miner&#xff1a;&#x6211;&#x60f3;&#x77e5;&#x9053;&#x8bb0;&#x5fc6;&#x7684;&#x6574;&#x4e2a;&#x6d41;&#x7a0b;&#x5728;&#x5206;&#x5b50;&#x5c42;&#x9762;&#x662f;&#x600e;&#x6837;&#x53d1;&#x751f;&#x7684;&#xff0c;&#x77ac;&#x65f6;&#x8bb0;&#x5fc6;&#x3001;&#x5de5;&#x4f5c;&#x8bb0;&#x5fc6;&#x3001;&#x957f;&#x671f;&#x8bb0;&#x5fc6;&#x3001;&#x9648;&#x8ff0;&#x6027;&#x8bb0;&#x5fc6;&#x3001;&#x7a0b;&#x5e8f;&#x6027;&#x8bb0;&#x5fc6;&#x7b49;&#x5404;&#x79cd;&#x8bb0;&#x5fc6;&#x80fd;&#x5728;&#x5206;&#x5b50;&#x5c42;&#x9762;&#x533a;&#x5206;&#x5417;&#xff1f; &#xff08;&#x5176;&#x4ed6;&#x4eba;&#x5462;&#xff1f;&#xff09;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1277886754404" ID="ID_1103849450" MODIFIED="1277887262232" TEXT="&#x4ece;&#x5206;&#x5b50;&#x901a;&#x8def;&#x770b;&#xff08;&#x5c31;&#x4ee5;&#x679c;&#x8747;&#x4e3a;&#x4f8b;&#x5427;&#xff09;"/>
+</node>
+<node CREATED="1277899175248" ID="ID_1849862158" MODIFIED="1277899366904" POSITION="right" TEXT="&#x7814;&#x7a76;&#x65b9;&#x6cd5;&#xff1a;&#x5149;&#x9057;&#x4f20;&#x5b66;&#xff08;Optogenetics&#xff09;">
+<node COLOR="#ff3399" CREATED="1277899455810" ID="ID_833019922" MODIFIED="1277899542967" STYLE="bubble" TEXT="&#x770b;&#x8fd9;&#x91cc;&#xff0c;&#x4e00;&#x7bc7;&#x725b;&#x4eba;&#x7684;&#x7efc;&#x8ff0;&#xff0c;&#x540e;&#x9762;&#x8fd8;&#x6709;&#x4e00;&#x4e9b;&#x54f2;&#x5b66;&#x63a2;&#x8ba8;"/>
+</node>
+<node CREATED="1277882241420" ID="ID_1096057920" MODIFIED="1277887784623" POSITION="left" TEXT="&#x53c2;&#x8003;&#x6587;&#x732e;">
+<node CREATED="1277897647685" ID="ID_443306389" MODIFIED="1277897785638" TEXT="&#x90a3;&#x672c;&#x53c8;&#x91cd;&#x53c8;&#x8d35;&#x7684;&#x5f69;&#x56fe;&#x4e66;&#xff08;&#x4e00;&#x65f6;&#x5931;&#x5fc6;&#x4e86;&#xff0c;&#x53eb;&#x5565;&#x6765;&#x7740;&#xff1f;&#xff09;"/>
+<node CREATED="1277897792170" ID="ID_1204959266" MODIFIED="1277899591592" TEXT="&#x4e00;&#x4e9b;&#x4e13;&#x4e1a;&#x6587;&#x732e;&#x3001;&#x7f51;&#x4e0a;&#x8d44;&#x6599;"/>
+</node>
+</node>
+</map>
