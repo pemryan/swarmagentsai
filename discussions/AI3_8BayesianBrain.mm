18e7f42c8bcdeac1baa13e7abaa66db94dcf6699
zmsearch <zmsearch@7bf8055b-b726-ec87-711b-5acdc8c35925>
2010-08-30 16:31:51 +0000

    Update AI3_1.mm, Add AI3_8BayesianBrain.mm

---
+++ discussions/AI3_8BayesianBrain.mm
@@ -0,0 +1,105 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1280628590764" ID="ID_436090705" MODIFIED="1280628868374" TEXT="Bayesian Brain">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node CREATED="1280628839264" ID="ID_142466907" MODIFIED="1280628863233" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Is this a unified theory of the brain?
+    </p>
+    <p>
+      Source: issue 2658 of New Scientist magazine, 28 May 2008, page 30-33
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1280629757655" ID="ID_1572800892" MODIFIED="1283067395843" POSITION="right" TEXT="history">
+<node CREATED="1280629774421" ID="ID_1135847214" MODIFIED="1280631036952" TEXT="1983, Geoffrey Hinton">
+<node CREATED="1280631048405" ID="ID_1826688509" MODIFIED="1283014132859" TEXT="brain could be seen as a machine making decisions based on the uncertainties of the outside world"/>
+</node>
+<node CREATED="1280631146218" ID="ID_818201777" MODIFIED="1280631360108" TEXT="1990s, Peter Dayan, Geoffrey Hinton &amp; Richard Zemel">
+<node CREATED="1280631199374" ID="ID_1932663883" MODIFIED="1280631200436" TEXT="brain represents knowledge of the world in terms of probabilities and made specific proposals for tractable neural processes that could manifest such a &apos;Helmholtz Machine&apos;"/>
+</node>
+<node CREATED="1280630180343" ID="ID_1460785163" MODIFIED="1280631602374" TEXT="1990s, Karl Friston &amp; Geoffrey Hinton">
+<node CREATED="1280631603358" ID="ID_1478231993" MODIFIED="1280631604483" TEXT="began examining the concept of &apos;free energy&apos;"/>
+</node>
+<node CREATED="1280630164343" ID="ID_1476381745" MODIFIED="1280631613764" TEXT="2005, Karl Friston">
+<node CREATED="1280631171968" ID="ID_1296354283" MODIFIED="1283050784125" TEXT="free energy principle - a unified brain theory : everything the brain does is designed to minimise free energy or prediction error"/>
+</node>
+</node>
+<node CREATED="1280631955436" ID="ID_204003932" MODIFIED="1280631959546" POSITION="right" TEXT="Persons">
+<node CREATED="1280633614358" ID="ID_1981978158" LINK="http://en.wikipedia.org/wiki/Thomas_Bayes" MODIFIED="1280633662999" TEXT="Thomas Bayes [1702-1761.4.17]">
+<node CREATED="1280633960686" ID="ID_433207182" MODIFIED="1280633961561" TEXT="Bayes&apos; theorem"/>
+</node>
+<node CREATED="1280631960593" ID="ID_1096678368" LINK="http://www.cs.toronto.edu/~hinton/" MODIFIED="1283065996718" TEXT="Geoffrey Hinton [1947.12.6-]">
+<node CREATED="1280632290483" ID="ID_1976800306" MODIFIED="1280632323374" TEXT="Education">
+<node CREATED="1280632304639" ID="ID_1488169917" MODIFIED="1280632305733" TEXT="BA in experimental psychology from Cambridge in 1970"/>
+<node CREATED="1280632277405" ID="ID_1516130164" MODIFIED="1280632314999" TEXT="PhD in Artificial Intelligence from Edinburgh in 1978"/>
+</node>
+<node CREATED="1280631969374" ID="ID_1496478285" MODIFIED="1280632067796" TEXT="AI &#x8fde;&#x63a5;&#x4e3b;&#x4e49;&#x7684;&#x4ee3;&#x8868;&#x4eba;&#x7269;"/>
+<node CREATED="1280631984436" ID="ID_1709873865" MODIFIED="1283014122766" TEXT="&#x4e3b;&#x8981;&#x5b66;&#x672f;&#x8d21;&#x732e;&#xff1a;back-propagation algorithm, Boltzmann machines, Helmholtz machines..."/>
+<node CREATED="1283048659718" ID="ID_1297791170" LINK="http://www.cs.utoronto.ca/~hinton/helmholtz.html" MODIFIED="1283048799250" TEXT="Helmholtz Machine">
+<node CREATED="1283048681906" ID="ID_1703354132" MODIFIED="1283048863250" TEXT="a class of neural networks which learn the hidden structure of a set of data by being trained to create a generative model which can produce the original set of data"/>
+<node CREATED="1283048738281" ID="ID_1820423880" MODIFIED="1283048748031" TEXT="by learning economical representations of the data, the underlying structure of the generative model should reasonably approximate the hidden structure of the data set. This is an unsupervised learning algorithm."/>
+</node>
+</node>
+<node CREATED="1280632240077" ID="ID_234815235" LINK="http://www.universaldarwinism.com/Friston%20Karl.htm" MODIFIED="1283185516765" TEXT="Karl Friston">
+<node CREATED="1283051490656" ID="ID_181815442" MODIFIED="1283051491921" TEXT="&#x8111;&#x529f;&#x80fd;&#x6210;&#x50cf;&#x5206;&#x6790;&#x8f6f;&#x4ef6;SPM(statistical parametric mapping)"/>
+<node CREATED="1283013163406" ID="ID_233931999" LINK="http://www.fil.ion.ucl.ac.uk/~karl/A%20free%20energy%20principle%20for%20the%20brain.pdf" MODIFIED="1283185546640" TEXT="free-energy principle">
+<node CREATED="1283047662671" ID="ID_55422970" MODIFIED="1283047668046" TEXT="Background">
+<node CREATED="1283014165547" ID="ID_781596169" LINK="http://en.wikipedia.org/wiki/Helmholtz" MODIFIED="1283048880281" TEXT="Helmholtz/&#x4ea5;&#x59c6;&#x970d;&#x5179; ">
+<node CREATED="1283014342437" ID="ID_39205697" MODIFIED="1283014343469" TEXT="1821-1894, &#x5fb7;&#x56fd;&#x7269;&#x7406;&#x5b66;&#x5bb6;&#x3001;&#x6570;&#x5b66;&#x5bb6;&#x3001;&#x751f;&#x7406;&#x5b66;&#x5bb6;&#x3001;&#x5fc3;&#x7406;&#x5b66;&#x5bb6;"/>
+<node CREATED="1283014355656" ID="ID_1855685850" MODIFIED="1283014356594" TEXT="free energy">
+<node CREATED="1283050217171" ID="ID_44051801" MODIFIED="1283050218265" TEXT="&#x628a;&#x5316;&#x5b66;&#x53cd;&#x5e94;&#x4e2d;&#x7684;&#x201c;&#x675f;&#x7f1a;&#x80fd;&#x201d;&#x548c;&#x201c;&#x81ea;&#x7531;&#x80fd;&#x2019;&#x533a;&#x522b;&#x5f00;&#x6765;&#xff0c;&#x6307;&#x51fa;&#x524d;&#x8005;&#x53ea;&#x80fd;&#x8f6c;&#x5316;&#x4e3a;&#x70ed;&#xff0c;&#x540e;&#x8005;&#x5374;&#x53ef;&#x4ee5;&#x8f6c;&#x5316;&#x4e3a;&#x5176;&#x4ed6;&#x5f62;&#x5f0f;&#x7684;&#x80fd;&#x91cf;&#x3002;"/>
+</node>
+</node>
+<node CREATED="1283052734343" ID="ID_1060284425" MODIFIED="1283052782062" TEXT="Richard Feynman / &#x7406;&#x67e5;&#x5fb7;&#xb7;&#x8d39;&#x66fc;">
+<node CREATED="1283052750500" ID="ID_945664519" MODIFIED="1283052772656" TEXT="1918-1988, &#x7f8e;&#x56fd;&#x7269;&#x7406;&#x5b66;&#x5bb6;"/>
+</node>
+<node CREATED="1283014456672" ID="ID_1616773882" LINK="http://itp1.uni-stuttgart.de/en/arbeitsgruppen/?W=5" MODIFIED="1283052786375" TEXT="Hermann Haken / &#x8d6b;&#x5c14;&#x66fc;&#x30fb;&#x54c8;&#x80af;">
+<node CREATED="1283014465234" ID="ID_1426735565" MODIFIED="1283016292172" TEXT="1927-, 1951&#x83b7;&#x6570;&#x5b66;&#x535a;&#x58eb;, &#x5fb7;&#x56fd;&#x7269;&#x7406;&#x5b66;&#x5bb6;, 1995&#x9000;&#x4f11;"/>
+<node CREATED="1283014521344" ID="ID_1123494241" MODIFIED="1283054730156" TEXT="Synergetics/&#x534f;&#x540c;&#x5b66;">
+<node CREATED="1283054734406" ID="ID_1792791135" MODIFIED="1283054754078" TEXT="&#x7cfb;&#x7edf;&#x79d1;&#x5b66;&#x8001;&#x4e09;&#x8bba;&#xff1a; &#x7cfb;&#x7edf;&#x8bba;&#x3001;&#x63a7;&#x5236;&#x8bba;&#x548c;&#x4fe1;&#x606f;&#x8bba;&#xff1b;&#x65b0;&#x4e09;&#x8bba;&#xff1a; &#x8017;&#x6563;&#x7ed3;&#x6784;&#x8bba;&#x3001;&#x534f;&#x540c;&#x8bba;&#x3001;&#x7a81;&#x53d8;&#x8bba;"/>
+<node CREATED="1283049721390" ID="ID_470700785" MODIFIED="1283049730781" TEXT="&#x6e90;&#x8d77;">
+<node CREATED="1283016382297" ID="ID_699433261" MODIFIED="1283016383625" TEXT="The interpretation of the laser principles as self organization of non equilibrium systems paved the way at the end of the 1960s to the development of synergetics"/>
+</node>
+<node CREATED="1283049654703" ID="ID_1360405008" MODIFIED="1283049656031" TEXT="&#x7814;&#x7a76;&#x534f;&#x540c;&#x7cfb;&#x7edf;&#x4ece;&#x65e0;&#x5e8f;&#x5230;&#x6709;&#x5e8f;&#x7684;&#x6f14;&#x5316;&#x89c4;&#x5f8b;">
+<node CREATED="1283049669906" ID="ID_845970681" MODIFIED="1283049685812" TEXT="&#x534f;&#x540c;&#x7cfb;&#x7edf;: &#x7531;&#x8bb8;&#x591a;&#x5b50;&#x7cfb;&#x7edf;&#x7ec4;&#x6210;&#x7684;&#x3001;&#x80fd;&#x4ee5;&#x81ea;&#x7ec4;&#x7ec7;&#x65b9;&#x5f0f;&#x5f62;&#x6210;&#x5b8f;&#x89c2;&#x7684;&#x7a7a;&#x95f4;&#x3001;&#x65f6;&#x95f4;&#x6216;&#x529f;&#x80fd;&#x6709;&#x5e8f;&#x7ed3;&#x6784;&#x7684;&#x5f00;&#x653e;&#x7cfb;&#x7edf;&#x3002;"/>
+<node CREATED="1283049793718" ID="ID_920643874" MODIFIED="1283049801390" TEXT="&#x4e3b;&#x8981;&#x662f;&#x7528;&#x6f14;&#x5316;&#x65b9;&#x7a0b;&#x6765;&#x7814;&#x7a76;&#x534f;&#x540c;&#x7cfb;&#x7edf;&#x7684;&#x5404;&#x79cd;&#x975e;&#x5e73;&#x8861;&#x5b9a;&#x6001;&#x548c;&#x4e0d;&#x7a33;&#x5b9a;&#x6027;(&#x53c8;&#x79f0;&#x975e;&#x5e73;&#x8861;&#x76f8;&#x53d8;)"/>
+<node CREATED="1283054855265" ID="ID_509043288" MODIFIED="1283054859687" TEXT="&#x5bf9;&#x975e;&#x8fdc;&#x79bb;&#x5e73;&#x8861;&#x6001;&#x7cfb;&#x7edf;&#x5b9e;&#x73b0;&#x7684;&#x7cfb;&#x7edf;&#x6f14;&#x5316;&#x63d0;&#x51fa;&#x4e86;&#x65b9;&#x6848;&#x3002;&#x54c8;&#x80af;&#x5728;&#x7814;&#x7a76;&#x4e2d;&#x53d1;&#x73b0;&#x6709;&#x5e8f;&#x7ed3;&#x6784;&#x7684;&#x51fa;&#x73b0;&#x4e0d;&#x4e00;&#x5b9a;&#x8981;&#x8fdc;&#x79bb;&#x5e73;&#x8861;&#xff0c;&#x7cfb;&#x7edf;&#x5185;&#x90e8;&#x8981;&#x7d20;&#x4e4b;&#x95f4;&#x534f;&#x540c;&#x52a8;&#x4f5c;&#x4e5f;&#x80fd;&#x591f;&#x5bfc;&#x81f4;&#x7cfb;&#x7edf;&#x6f14;&#x5316;&#xff08;&#x5185;&#x56e0;&#x5bf9;&#x4e8e;&#x7cfb;&#x7edf;&#x6f14;&#x5316;&#x7684;&#x4ef7;&#x503c;&#x548c;&#x9014;&#x5f84;&#xff09;&#x3002;&#x4ed6;&#x8ba4;&#x8bc6;&#x5230;&#x71b5;&#x6982;&#x5ff5;&#x7684;&#x5c40;&#x9650;&#x6027;&#xff0c;&#x63d0;&#x51fa;&#x4e86;&#x5e8f;&#x53c2;&#x91cf;&#x7684;&#x6982;&#x5ff5;&#x3002;&#x5e8f;&#x53c2;&#x91cf;&#x662f;&#x7cfb;&#x7edf;&#x901a;&#x8fc7;&#x5404;&#x8981;&#x7d20;&#x7684;&#x534f;&#x540c;&#x4f5c;&#x7528;&#x800c;&#x5f62;&#x6210;&#xff0c;&#x540c;&#x65f6;&#x5b83;&#x53c8;&#x652f;&#x914d;&#x7740;&#x5404;&#x4e2a;&#x5b50;&#x7cfb;&#x7edf;&#x7684;&#x884c;&#x4e3a;&#x3002;&#x5e8f;&#x53c2;&#x91cf;&#x662f;&#x7cfb;&#x7edf;&#x4ece;&#x65e0;&#x5e8f;&#x5230;&#x6709;&#x5e8f;&#x53d8;&#x5316;&#x53d1;&#x5c55;&#x7684;&#x4e3b;&#x5bfc;&#x56e0;&#x7d20;&#xff0c;&#x5b83;&#x51b3;&#x5b9a;&#x7740;&#x7cfb;&#x7edf;&#x7684;&#x81ea;&#x7ec4;&#x7ec7;&#x884c;&#x4e3a;&#x3002;&#x5f53;&#x7cfb;&#x7edf;&#x5904;&#x4e8e;&#x6df7;&#x4e71;&#x7684;&#x72b6;&#x6001;&#x65f6;&#xff0c;&#x5176;&#x5e8f;&#x53c2;&#x91cf;&#x4e3a;&#x96f6;&#xff1b;&#x5f53;&#x7cfb;&#x7edf;&#x5f00;&#x59cb;&#x51fa;&#x73b0;&#x6709;&#x5e8f;&#x65f6;&#xff0c;&#x5e8f;&#x53c2;&#x91cf;&#x4e3a;&#x975e;&#x96f6;&#x503c;&#xff0c;&#x5e76;&#x4e14;&#x968f;&#x7740;&#x5916;&#x754c;&#x6761;&#x4ef6;&#x7684;&#x6539;&#x5584;&#x548c;&#x7cfb;&#x7edf;&#x6709;&#x5e8f;&#x7a0b;&#x5ea6;&#x7684;&#x63d0;&#x9ad8;&#x800c;&#x9010;&#x6e10;&#x589e;&#x5927;&#xff0c;&#x5f53;&#x63a5;&#x8fd1;&#x4e34;&#x754c;&#x70b9;&#x65f6;&#xff0c;&#x5e8f;&#x53c2;&#x91cf;&#x6025;&#x5267;&#x589e;&#x5927;&#xff0c;&#x6700;&#x7ec8;&#x5728;&#x4e34;&#x754c;&#x57df;&#x7a81;&#x53d8;&#x5230;&#x6700;&#x5927;&#x503c;&#xff0c;&#x5bfc;&#x81f4;&#x7cfb;&#x7edf;&#x4e0d;&#x7a33;&#x5b9a;&#x800c;&#x53d1;&#x751f;&#x7a81;&#x53d8;&#x3002;&#x5e8f;&#x53c2;&#x91cf;&#x7684;&#x7a81;&#x53d8;&#x610f;&#x5473;&#x7740;&#x5b8f;&#x89c2;&#x65b0;&#x7ed3;&#x6784;&#x51fa;&#x73b0;&#x3002;"/>
+<node CREATED="1283049596687" ID="ID_708198727" MODIFIED="1283049597796" TEXT="&#x5f79;&#x4f7f;&#x539f;&#x7406;&#xff08;slaving principle&#xff09;">
+<node CREATED="1283049623421" ID="ID_297663488" MODIFIED="1283049624640" TEXT="&#x7cfb;&#x7edf;&#x6f14;&#x5316;&#x8fc7;&#x7a0b;&#x4e2d;&#xff0c;&#x5728;&#x63a5;&#x8fd1;&#x72b6;&#x6001;&#x53d8;&#x5316;&#x7684;&#x4e34;&#x754c;&#x70b9;&#x65f6;&#xff0c;&#x201c;&#x5feb;&#x53d8;&#x91cf;&#x201d;&#x7531;&#x4e8e;&#x53d8;&#x5316;&#x592a;&#x5feb;&#xff0c;&#x4ee5;&#x81f4;&#x5728;&#x672a;&#x5bf9;&#x7cfb;&#x7edf;&#x65bd;&#x52a0;&#x5f71;&#x54cd;&#x4ee5;&#x524d;&#x5c31;&#x6d88;&#x5931;&#x6216;&#x53d8;&#x5316;&#x4e86;&#xff0c;&#x800c;&#x6781;&#x5c11;&#x6570;&#x201c;&#x6162;&#x53d8;&#x91cf;&#x201d;&#x53d8;&#x5316;&#x76f8;&#x5bf9;&#x7f13;&#x6162;&#xff0c;&#x6210;&#x4e3a;&#x652f;&#x914d;&#x548c;&#x4e3b;&#x5bb0;&#x7cfb;&#x7edf;&#x6f14;&#x5316;&#x7684;&#x5e8f;&#x53c2;&#x91cf;&#x3002;&#x5e8f;&#x53c2;&#x91cf;&#x7531;&#x5b50;&#x7cfb;&#x7edf;&#x7684;&#x7ade;&#x4e89;&#x4e0e;&#x534f;&#x540c;&#x4ea7;&#x751f;&#x51fa;&#x6765;&#xff0c;&#x540c;&#x65f6;&#x5b83;&#x53c8;&#x652f;&#x914d;&#x5b50;&#x7cfb;&#x7edf;&#xff0c;&#x5b50;&#x7cfb;&#x7edf;&#x4f3a;&#x670d;&#x4e8e;&#x5e8f;&#x53c2;&#x91cf;&#x3001;&#x5e8f;&#x53c2;&#x91cf;&#x534f;&#x540c;&#x5408;&#x4f5c;&#x5f62;&#x6210;&#x6709;&#x5e8f;&#x7684;&#x5b8f;&#x89c2;&#x7ed3;&#x6784;"/>
+</node>
+</node>
+<node CREATED="1283049711453" ID="ID_687666119" MODIFIED="1283049713875" TEXT="books">
+<node CREATED="1283017664469" ID="ID_1931843496" MODIFIED="1283049439453" TEXT="&lt;&#x534f;&#x540c;&#x5b66;&#xff1a;&#x5927;&#x81ea;&#x7136;&#x6784;&#x6210;&#x7684;&#x5965;&#x79d8;(1981, 1995~2005&#x4e2d;)&gt;"/>
+<node CREATED="1283017674656" ID="ID_268826567" MODIFIED="1283017772641" TEXT="&lt;&#x5927;&#x8111;&#x5de5;&#x4f5c;&#x539f;&#x7406;&#xff1a;&#x8111;&#x6d3b;&#x52a8;&#x3001;&#x884c;&#x4e3a;&#x548c;&#x8ba4;&#x77e5;&#x7684;&#x534f;&#x540c;&#x5b66;&#x7814;&#x7a76; (1996, 2000&#x4e2d;)&gt;">
+<node CREATED="1283016179609" ID="ID_351726234" MODIFIED="1283016180531" TEXT="&#x5927;&#x8111;&#x662f;&#x4e00;&#x4e2a;&#x4ee5;&#x7269;&#x7406;&#x5b66;&#x5b9a;&#x5f8b;&#x4e3a;&#x57fa;&#x7840;&#x7684;&#x590d;&#x6742;&#x5de8;&#x7cfb;&#x7edf;&#xff0c;&#x7ecf;&#x7531;&#x4e0d;&#x540c;&#x5c42;&#x6b21;&#x7684;&#x81ea;&#x7ec4;&#x7ec7;&#x8fc7;&#x7a0b;&#x8c03;&#x8282;&#x63a7;&#x5236;&#x53c2;&#x91cf;"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1283047718718" ID="ID_1084158481" MODIFIED="1283070512171" TEXT="contents">
+<node CREATED="1283047723312" ID="ID_1086990067" MODIFIED="1283047733265" TEXT="the free-energy principle"/>
+<node CREATED="1283047737656" ID="ID_505048476" MODIFIED="1283047749500" TEXT="the Bayesian brain hypothesis"/>
+<node CREATED="1283047754531" ID="ID_1862001528" MODIFIED="1283047763875" TEXT="the principle of efficient coding"/>
+<node CREATED="1283050350937" ID="ID_593141466" MODIFIED="1283050358515" TEXT="other theories">
+<node CREATED="1283047846875" ID="ID_45194771" MODIFIED="1283047858359" TEXT="the cell assembly and correlation theory"/>
+<node CREATED="1283047858828" ID="ID_607526543" MODIFIED="1283047869671" TEXT="biased competition and attention"/>
+<node CREATED="1283047874453" ID="ID_1140779110" MODIFIED="1283047884421" TEXT="Neural Darwinism and value learning"/>
+<node CREATED="1283047885640" ID="ID_1342422871" MODIFIED="1283047896281" TEXT="optimal control theory and game theory"/>
+</node>
+<node CREATED="1283047903234" ID="ID_1477611425" MODIFIED="1283047912421" TEXT="conclusions and future directions">
+<node CREATED="1283047927046" ID="ID_1624696581" MODIFIED="1283047976843" TEXT="many global theories of brain function can be united under a Helmholtzian perceptive of the brain as a generative model of the world it inhabits"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
